6cae7088748985afacf7156171d139073b5b5d78
aashanamir <emailstormfacts@gmail.com>
2024-03-26 12:29:28 +0500

    First Lecture Completed

---
+++ Chapter 1/script.js
@@ -0,0 +1,68 @@
+// 5. let, const, and var
+
+// 1 Var
+var myVar = "MY VAriable";
+
+var myVar = "var ki problem"
+
+var myVar;
+
+myVar = "var esy bhi chlta hy";
+
+// console.log(myVar);
+
+// 2 Let
+
+let myName = "Muhammad Aashan";
+
+let my2Var = "Hello";
+
+my2Var = "let bhi esy chl jata hy";
+
+// console.log(my2Var);
+
+
+//3 const
+
+const myConst = "This is const";
+
+// myConst = "dkkdk"; wrong syntax
+
+// console.log(myConst);
+
+// const my2Const ; initialization is mendotary while declaring the constant variable
+
+/** *************
+ * *************
+ * Other Portion
+ * *************
+ */
+// 6. Data Types in JavaScript#
+{
+    let myName = "Aashan Amir";
+    let myNum = 12;
+    let myBool = true;
+    let myUnDef = undefined;
+    let myNull = null;
+
+    console.log( typeof myName);
+    console.log(  typeof myNum);
+    console.log( typeof myBool);
+    console.log( typeof myUnDef);
+    console.log( typeof myNull);
+
+}
+
+
+const myObj = {
+    name : "Road Prince",
+    model : 18,
+    millage : 50,
+    isSulf : false,
+}
+let objName = myObj.name;
+let objModel = myObj.model;
+let objMillage = myObj.millage;
+let objSulf = myObj.isSulf;
+
+console.log(objName ,  objModel , objMillage , objSulf , typeof myObj);
