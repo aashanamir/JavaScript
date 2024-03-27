4f8af96ff7978c399b79028e594abb0d82e78890
aashanamir <emailstormfacts@gmail.com>
2024-03-27 13:21:14 +0500

    lesson 2 completed

--- Chapter 2/script.js
+++ Chapter 2/script.js
@@ -66,4 +66,91 @@ spanning multiple lines
     // != (not equal to)
     console.log(a !== str); // true
 
+
+    // > (greater than)
+    console.log(18 > 18); // true
+
+
+    // > (less than)
+    console.log(10 < 5); // False
+
+
+    // >= (greater than or equal to)
+    console.log(5 >= 5); // true
+
+    // <= (less than or equal to)
+    console.log(5 <= 5); // true
+}
+
+
+// 6. Conditional Statements (if, else if, and else)
+
+// {
+//     let a = 10;
+//     if (a < 10)//false
+//     {
+//         alert(a + " is less than 10");
+//     }
+//     else if (a === 9) //false
+//     {
+//         alert(a + " is equal to 9");
+//     }
+//     else if (a === 10) //true
+//     {
+//         alert(a + " is equal to 10");
+//     }
+//     else {
+//         alert(a + " is greater than 10");
+//     }
+
+// }
+
+
+
+// 7. Ternory Operators
+
+{
+    // let age = prompt("Enter Your Age");
+    // let canVote = (age >= 18) ? " greater than 18  " : "No";
+    // document.write(canVote); // "Yes"
+
+    let num = 10;
+    let result = (num % 2 === 0) ? "Even" : "Odd";
+    console.log(result); // "Even"
+}
+
+{
+    let myVar = 6;
+
+    switch (myVar) {
+
+        case 1: console.log("Incorrect option");
+            break;
+
+        case 2: console.log("This is also incorrect");
+            break;
+
+
+        case 3: console.log("This is 3 also incorrect");
+            break;
+
+        default: console.log("Input is incorrect");
+
+
+    }
+}
+
+
+{
+    let age = 18;
+    let height = 5;
+    let minAge = 18;
+    let minHeight = 5;
+
+    if (age >= minAge && height >= minHeight) {
+        document.write("You are eligible to apply");
+    }
+    else {
+        document.write("You are not eligible to apply");
+    }
 }
